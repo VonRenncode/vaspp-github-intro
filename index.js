c69cdf03f1d48bdbbaeb1ccba5b298f900e7c071
VonRenncode <83127999+VonRennstein@users.noreply.github.com>
2024-05-03 14:02:02 +0200

    Created Workshop Repo
    Created Workshop Repo and little assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function welcome() {
+    console.log("Welcome to our GitHub Workshop! The following people finished the assignment: ");
+    // kevin();
+}
+
+welcome();
+
+// Add functions that output your name below and dont forget to include it in the welcome function
+
+// function kevin() {
+//     console.log("Kevin");
+// };
